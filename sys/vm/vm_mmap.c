602d7d8da3c9c90098a3f9c59add6e32b69590c6
Shawn Webb <shawn.webb@hardenedbsd.org>
2016-03-21 21:02:14 -0400

    HBSD OPNsense: Separate out the ASLR code.
    On OPNsense's 16.7 roadmap is HardenedBSD's ASLR code. This commit
    separates out the ASLR code from the rest of our exploit mitigation
    and system hardening code.
    
    Testing and verification still need to be performed. Initial testing
    (compile + boot + `procstat -v PIDofPIEapplication) has been
    performed. More thorough testing should occur.
    
    Shared object load order randomization in the RTLD is not included in
    this patch. That will be discussed with the fine folks at OPNsense at
    a later time.
    
    Since OPNsense is based on FreeBSD 10.x, this patch will need to be
    backported to 10-STABLE. However, a "horizontal port" to 11-CURRENT,
    which is what this commit is, needed to be done first.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- sys/vm/vm_mmap.c
+++ sys/vm/vm_mmap.c
@@ -45,6 +45,7 @@ __FBSDID("$FreeBSD$");
 
 #include "opt_compat.h"
 #include "opt_hwpmc_hooks.h"
+#include "opt_pax.h"
 #include "opt_vm.h"
 
 #include <sys/param.h>
@@ -55,6 +56,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/mutex.h>
 #include <sys/sysproto.h>
 #include <sys/filedesc.h>
+#include <sys/pax.h>
 #include <sys/priv.h>
 #include <sys/proc.h>
 #include <sys/procctl.h>
@@ -193,11 +195,14 @@ sys_mmap(td, uap)
 	struct file *fp;
 	vm_offset_t addr;
 	vm_size_t size, pageoff;
-	vm_prot_t cap_maxprot;
-	int align, error, flags, prot;
+	vm_prot_t cap_maxprot, prot;
+	int align, error, flags;
 	off_t pos;
 	struct vmspace *vms = td->td_proc->p_vmspace;
 	cap_rights_t rights;
+#ifdef PAX_ASLR
+	int pax_aslr_done;
+#endif
 
 	addr = (vm_offset_t) uap->addr;
 	size = uap->len;
@@ -206,6 +211,10 @@ sys_mmap(td, uap)
 	pos = uap->pos;
 
 	fp = NULL;
+
+#ifdef PAX_ASLR
+	pax_aslr_done = 0;
+#endif
 
 	/*
 	 * Ignore old flags that used to be defined but did not do anything.
@@ -302,7 +311,13 @@ sys_mmap(td, uap)
 		 */
 		if (addr + size > MAP_32BIT_MAX_ADDR)
 			addr = 0;
-#endif
+#ifdef PAX_ASLR
+		PROC_LOCK(td->td_proc);
+		pax_aslr_mmap_map_32bit(td->td_proc, &addr, (vm_offset_t)uap->addr, flags);
+		pax_aslr_done = 1;
+		PROC_UNLOCK(td->td_proc);
+#endif /* PAX_ASLR */
+#endif /* MAP_32BIT */
 	} else {
 		/*
 		 * XXX for non-fixed mappings where no hint is provided or
@@ -318,6 +333,12 @@ sys_mmap(td, uap)
 		    lim_max(td, RLIMIT_DATA))))
 			addr = round_page((vm_offset_t)vms->vm_daddr +
 			    lim_max(td, RLIMIT_DATA));
+#ifdef PAX_ASLR
+		PROC_LOCK(td->td_proc);
+		pax_aslr_mmap(td->td_proc, &addr, (vm_offset_t)uap->addr, flags);
+		pax_aslr_done = 1;
+		PROC_UNLOCK(td->td_proc);
+#endif
 	}
 	if (size == 0) {
 		/*
